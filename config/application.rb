19f7cd12281f709aba04906576c6772ade470535
Mohd Sameer <mohdsameer1310@gmail.com>
2016-11-10 00:43:54 +0530

    fixed

--- config/application.rb
+++ config/application.rb
@@ -75,8 +75,7 @@ module Kassi
       'app-bundle.css',
       'app-bundle.js',
       'vendor-bundle.js',
-      'landing_page_new/base.css',
-      'landing_page_new/base.js'
+      'landing_page_new/*'
     ]
 
     # Read the config from the config.yml
